42d92287d756c21f2a68db602a2b6ee5236788d8
Scott Frazer <scott.d.frazer@gmail.com>
2012-09-18 15:46:41 -0400

    renaming test file

---
+++ test/org/broadinstitute/compositetask/CompositeTaskTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.*;
+import org.broadinstitute.compositetask.CompositeTask;
+
+public class CompositeTaskTest 
+{
+    @Test 
+    public void test()
+    {
+        
+    }
+}
