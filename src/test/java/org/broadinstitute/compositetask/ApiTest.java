d8c4784fb450c95b624b78010cc19993df25e6f8
Scott Frazer <scott.d.frazer@gmail.com>
2013-09-20 22:22:34 -0400

    Add more tests, refactor existing tests

--- src/test/java/org/broadinstitute/compositetask/ApiTest.java
+++ src/test/java/org/broadinstitute/compositetask/ApiTest.java
@@ -31,22 +31,54 @@ public class ApiTest
 
     @Test
     public void testGetNodesMethodReturnsTheRightNumberOfNodes() {
-        Set<CompositeTaskNode> nodes = this.task.getNodes();
-        Assert.assertEquals(nodes.size(), 2, "Expecting 2 nodes, found " + nodes.size());
+        Assert.assertEquals(this.task.getNodes().size(), 2, "Expecting 2 nodes, found " + this.task.getNodes().size());
     }
 
     @Test
-    public void testGetNodesMethodReturnsASingleTopLevelStepNode() {
+    public void testGetStepMethodReturnsTheCorrectTopLevelStep() {
         CompositeTaskStep step = this.task.getStep("atask");
         Assert.assertNotNull(step, "Expected one step with name 'atask'");
         Assert.assertEquals(step.getName(), "atask", "Expecting one step with name 'atask'");
+        Assert.assertEquals(step.getParent(), this.task, "Expecting the parent of 'atask' step to be the composite task");
     }
 
     @Test
-    public void testGetNodesMethodReturnsASingleTopLevelForLoopNode() {
+    public void testTopLevelStepHasCorrectOutputMappingsToVariableX() {
+        CompositeTaskStep step = this.task.getStep("atask");
+        boolean found = false;
+        for ( CompositeTaskStepOutput output : step.getOutputs() ) {
+            if ( output.getVariable().getName().equals("x") ) {
+                found = true;
+                Assert.assertEquals(output.getType(), "File", "Expected the variable 'x' to be a File mapping");
+                Assert.assertEquals(output.getMethod(), "assign", "Expected the mapping method to be 'assign'");
+                Assert.assertEquals(output.getPath(), "foo.txt", "Expected the output file path to be 'foo.txt'");
+            }
+        }
+        if ( !found ) Assert.fail("Expected to find one output to 'atask' to variable 'x'");
+    }
+
+    @Test
+    public void testTopLevelStepHasCorrectOutputMappingsToVariableY() {
+        CompositeTaskStep step = this.task.getStep("atask");
+        boolean found = false;
+        for ( CompositeTaskStepOutput output : step.getOutputs() ) {
+            if ( output.getVariable().getName().equals("y") ) {
+                found = true;
+                Assert.assertEquals(output.getType(), "FirstLine", "Expected the variable 'y' to be a File mapping");
+                Assert.assertEquals(output.getMethod(), "assign", "Expected the mapping method to be 'assign'");
+                Assert.assertEquals(output.getPath(), "bar.txt", "Expected the output file path to be 'bar.txt'");
+            }
+        }
+        if ( !found ) Assert.fail("Expected to find one output to 'atask' to variable 'y'");
+    }
+
+    @Test
+    public void testSingleTopLevelForLoopExistsWithSubTask() {
         Set<CompositeTaskNode> nodes = this.task.getNodes();
         CompositeTaskStep innerStep = this.task.getStep("btask");
+        Assert.assertNotNull(innerStep, "Expected a step with name 'btask' to exist");
         CompositeTaskForLoop loop = (CompositeTaskForLoop) innerStep.getParent();
+        Assert.assertEquals(loop.getParent(), this.task, "Expected the loop's parent node to be the composite task");
         Assert.assertEquals(loop.getCollection().getName(), "foobar", "Expecting one loop with a collection called 'foobar'");
         Assert.assertEquals(loop.getVariable().getName(), "item", "Expecting one loop with a variable called 'item'");
     }
