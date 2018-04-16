c597dbb086f49cccdc684d3349a6d1fa33d8c85d
Ohad Rodeh <orodeh@dnanexus.com>
2018-04-16 19:02:23 +0000

    Merging with latest openwdl version

--- versions/draft-3/parsers/java/download.sh
+++ versions/draft-3/parsers/java/download.sh
@@ -1,14 +1,10 @@
 COMMONS_CODEC='commons-codec-1.10'
 COMMONS_LANG='commons-lang3-3.4'
 
-if [ ! -f ${COMMONS_LANG}.jar ]; then
-  curl -L http://apache.spinellicreations.com//commons/lang/binaries/${COMMONS_LANG}-bin.tar.gz | tar xvz
-  cp ${COMMONS_LANG}/${COMMONS_LANG}.jar .
-  rm -rf ${COMMONS_LANG}
+if [ ! -f ${COMMONS_CODEC}.jar ]; then
+  wget https://repo1.maven.org/maven2/commons-codec/commons-codec/1.10/${COMMONS_CODEC}.jar
 fi
 
-if [ ! -f ${COMMONS_CODEC}.jar ]; then
-  curl -L http://mirror.metrocast.net/apache//commons/codec/binaries/${COMMONS_CODEC}-bin.tar.gz | tar xvz
-  cp ${COMMONS_CODEC}/${COMMONS_CODEC}.jar .
-  rm -rf ${COMMONS_CODEC}
+if [ ! -f ${COMMONS_LANG}.jar ]; then
+  wget http://central.maven.org/maven2/com/hynnet/commons-lang3/3.4/${COMMONS_LANG}.jar
 fi
