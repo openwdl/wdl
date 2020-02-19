58e4998f453d1da70b67390f1684779669d21071
patrick <patrick@dnastack.com>
2020-02-19 10:34:51 -0500

    Fixed versions of parsers

--- versions/1.0/parsers/antlr4/Python/setup.py
+++ versions/1.0/parsers/antlr4/Python/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='WdlParser',
-      version='development',
+      version='1.0',
       description='WDL parser',
       author='Patrick Magee',
       author_email='patrickmageee@gmail.com',
